3fc8083efd471044e5b945ae0123451205e644bb
Tom Sandt <tomsandt206@gmail.com>
2024-02-21 13:34:12 +0100

    added WebConfig

---
+++ src/main/java/de/tom/scheduler/config/WebConfig.java
@@ -0,0 +1,26 @@
+package de.tom.scheduler.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+                response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+                response.setHeader("Pragma", "no-chache");
+                response.setHeader("Expires", "0");
+            }
+        });
+    }
+
+}
